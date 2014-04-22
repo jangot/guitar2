3d05ba38351bca00e29b258c35d6fa0024606150
Pavel Pulin <pulin.pavel@gmail.com>
2014-04-22 21:24:46 +0400

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     var path = require('path');
 
     grunt.initConfig({
+        
         rootPath: path.resolve('.'),
         publicPath: '<%= rootPath %>/public',
         compilePath: '<%= rootPath %>/compile',
